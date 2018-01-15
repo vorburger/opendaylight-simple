62f3997be8eaf0e26f49d9824b1ff96765f9ae95
Michael Vorburger <mike@vorburger.ch>
2018-01-15 23:02:46 +0100

    add skeleton
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ src/main/java/org/opendaylight/netvirt/simple/NetvirtModule.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.netvirt.simple;
+
+import org.opendaylight.infrautils.inject.guice.testutils.AbstractGuiceJsr250Module;
+
+/**
+ * Guice Module with Bindings for netvirt services.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class NetvirtModule extends AbstractGuiceJsr250Module {
+
+    @Override
+    protected void configureBindings() throws Exception {
+    }
+
+}
