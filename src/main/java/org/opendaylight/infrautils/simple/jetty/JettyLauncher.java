be5208d6bc1f3d5cb21dcf8b4e9e0e966f0f1fae
Michael Vorburger <mike@vorburger.ch>
2018-02-07 22:12:14 +0100

    make JettyLauncher able to handle multiple web.xml and contexts
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- src/main/java/org/opendaylight/infrautils/simple/jetty/JettyLauncher.java
+++ src/main/java/org/opendaylight/infrautils/simple/jetty/JettyLauncher.java
@@ -7,13 +7,15 @@
  */
 package org.opendaylight.infrautils.simple.jetty;
 
+import com.google.errorprone.annotations.Var;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
-import java.util.LinkedList;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.inject.Singleton;
@@ -59,34 +61,14 @@ public class JettyLauncher {
     @PostConstruct
     @SuppressWarnings("checkstyle:IllegalThrows") // Jetty WebAppContext.getUnavailableException() throws Throwable
     public void start() throws Throwable {
-        WebAppContext webApp = new WebAppContext();
-        webApp.getServletHandler().setStartWithUnavailable(false);
-        webApp.setThrowUnavailableOnStartupException(true);
-        webApp.setLogUrlOnStart(true);
-        server.setHandler(webApp);
-
-        // TODO read this from... Web-ContextPath from MANIFEST.MF
-        webApp.setContextPath("/test");
-
-        // TODO LOG.info baseResource, with context..
-        webApp.setBaseResource(chop(webXmlUrl(), WEB_INF_WEB_XML));
-        LOG.info("webApp.getWebInf() = {}", webApp.getWebInf());
-        // TODO not needed? webApp.setDescriptor("WEB-INF/web.xml");
-        // TODO or? webApp.setDescriptor(JettyLauncher.class.getResource("/WEB-INF/web.xml").toString());
-
-        // see https://www.eclipse.org/jetty/documentation/9.3.x/jetty-classloading.html
-        webApp.setParentLoaderPriority(true);
-        // TODO need this, or delete?
-        // "This will make EVERYTHING on the classpath be
-        // scanned for META-INF/resources and web-fragment.xml - great for dev!
-        // NOTE: Several patterns can be listed, separate by comma"
-        // ? webApp.setAttribute(WebInfConfiguration.CONTAINER_JAR_PATTERN, ".*");
-
+        List<WebAppContext> webAppContexts = addWebAppContexts();
         server.start();
 
-        Throwable unavailableException = webApp.getUnavailableException();
-        if (unavailableException != null) {
-            throw unavailableException;
+        for (WebAppContext webAppContext : webAppContexts) {
+            Throwable unavailableException = webAppContext.getUnavailableException();
+            if (unavailableException != null) {
+                throw unavailableException;
+            }
         }
     }
 
@@ -100,36 +82,55 @@ public class JettyLauncher {
     // and http://blog2.vorburger.ch/2015/03/mifos-standalone-package-how-to.html
     // and http://blog2.vorburger.ch/2014/09/mifos-executable-war-with-mariadb4j.html
 
-    private Resource chop(final URL baseURL, String toChop) throws MalformedURLException, IOException {
-        String base = baseURL.toExternalForm();
+    private List<WebAppContext> addWebAppContexts() throws IOException {
+        @Var int temporaryToRemove = 1;
+        List<WebAppContext> webAppContexts = new ArrayList<>();
+        for (URL webXmlUrl : getResources(WEB_INF_WEB_XML)) {
+            String baseResourceURL = chop(webXmlUrl.toExternalForm(), WEB_INF_WEB_XML);
+            Resource baseResource = Resource.newResource(baseResourceURL);
+
+            WebAppContext webAppContext = new WebAppContext();
+            // TODO LOG.info baseResource, with context..
+            webAppContext.setBaseResource(baseResource);
+
+            // TODO read this from... Web-ContextPath from MANIFEST.MF
+            String contextPath = "/test" + temporaryToRemove++;
+            webAppContext.setContextPath(contextPath);
+            LOG.info("Found web.xml; adding {} from {}", contextPath, baseResourceURL);
+
+            webAppContext.getServletHandler().setStartWithUnavailable(false);
+            webAppContext.setThrowUnavailableOnStartupException(true);
+            webAppContext.setLogUrlOnStart(true);
+            server.setHandler(webAppContext);
+
+            LOG.debug("webApp.getWebInf() = {}", webAppContext.getWebInf());
+            // TODO not needed? webApp.setDescriptor("WEB-INF/web.xml");
+            // TODO or? webApp.setDescriptor(JettyLauncher.class.getResource("/WEB-INF/web.xml").toString());
+
+            // see https://www.eclipse.org/jetty/documentation/9.3.x/jetty-classloading.html
+            webAppContext.setParentLoaderPriority(true);
+            // TODO need this, or delete?
+            // "This will make EVERYTHING on the classpath be
+            // scanned for META-INF/resources and web-fragment.xml - great for dev!
+            // NOTE: Several patterns can be listed, separate by comma"
+            // ? webApp.setAttribute(WebInfConfiguration.CONTAINER_JAR_PATTERN, ".*");
+
+            webAppContexts.add(webAppContext);
+        }
+        return webAppContexts;
+    }
+
+    private String chop(String base, String toChop) throws MalformedURLException, IOException {
         if (!base.endsWith(toChop)) {
             throw new IllegalArgumentException(base + " does not endWith " + toChop);
         }
-        base = base.substring(0, base.length() - toChop.length());
-        return Resource.newResource(base);
+        return base.substring(0, base.length() - toChop.length());
     }
 
-    private static URL webXmlUrl() throws IOException {
-        final Collection<URL> urls = getResources(WEB_INF_WEB_XML);
-        if (urls.isEmpty()) {
-            throw new IllegalStateException(WEB_INF_WEB_XML + " not found on the classpath");
-        }
-// TODO support multiple..
-//        if (urls.size() != 1) {
-//            throw new IllegalStateException(
-//                    WEB_INF_WEB_XML + " was found more than once on the classpath: " + urls.toString());
-//        }
-        return (URL) urls.toArray()[0];
-    }
-
-    private static Collection<URL> getResources(String resource) throws IOException {
-        final ClassLoader cl = JettyLauncher.class.getClassLoader();
-        final Enumeration<URL> urls = cl.getResources(resource);
-        final LinkedList<URL> list = new LinkedList<>();
-        while (urls.hasMoreElements()) {
-            list.add(urls.nextElement());
-        }
-        return list;
+    private static List<URL> getResources(String resource) throws IOException {
+        ClassLoader cl = JettyLauncher.class.getClassLoader();
+        Enumeration<URL> urls = cl.getResources(resource);
+        return Collections.list(urls);
     }
 
 }
