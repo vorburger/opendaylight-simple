4345fe45cb0ab7cd2abe8f3c2ee3f3805dc296a7
Michael Vorburger <vorburger@redhat.com>
2018-02-13 22:50:51 +0100

    OsgiServicesWiring for HttpService (with throws TODO)
    used in currently still broken AAA; may remove this again later

---
+++ src/main/java/org/opendaylight/infrautils/simple/osgi/HttpServiceImpl.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.simple.osgi;
+
+import java.util.Dictionary;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import org.osgi.service.http.HttpContext;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
+
+/**
+ * Simple {@link HttpService} implementation.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class HttpServiceImpl implements HttpService {
+
+    @Override
+    public void registerServlet(String alias, Servlet servlet, @SuppressWarnings("rawtypes") Dictionary initparams,
+            HttpContext context) throws ServletException, NamespaceException {
+        throw new UnsupportedOperationException("TODO Implement me... ;)");
+    }
+
+    @Override
+    public void registerResources(String alias, String name, HttpContext context) throws NamespaceException {
+        throw new UnsupportedOperationException("TODO Implement me... ;)");
+    }
+
+    @Override
+    public void unregister(String alias) {
+        throw new UnsupportedOperationException("TODO Implement me... ;)");
+    }
+
+    @Override
+    public HttpContext createDefaultHttpContext() {
+        throw new UnsupportedOperationException("TODO Implement me... ;)");
+    }
+
+}
