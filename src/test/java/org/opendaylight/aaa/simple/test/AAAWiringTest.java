4874bb13b74e1af6f8e63b4511a8a65ed9d65360
Michael Vorburger <vorburger@redhat.com>
2018-02-13 22:51:18 +0100

    {WIP} AAAWiring incl. Test
    but it gets stuck (dead locked) in
    org.opendaylight.aaa.shiro.web.env.KarafIniWebEnvironment.init()

---
+++ src/test/java/org/opendaylight/aaa/simple/test/AAAWiringTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.aaa.simple.test;
+
+import org.junit.Rule;
+import org.opendaylight.aaa.simple.AAAWiring;
+import org.opendaylight.controller.simple.ControllerWiring;
+import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
+import org.opendaylight.infrautils.simple.jetty.WebWiring;
+import org.opendaylight.infrautils.simple.osgi.OsgiServicesWiring;
+import org.opendaylight.infrautils.simple.testutils.AbstractSimpleDistributionTest;
+
+public class AAAWiringTest extends AbstractSimpleDistributionTest {
+
+    public @Rule GuiceRule guice = new GuiceRule(AAAWiring.class, ControllerWiring.class, WebWiring.class,
+            OsgiServicesWiring.class, AnnotationsModule.class);
+
+}
