e4a0cb3832e3485616574d54ed030798cf83a644
Michael Vorburger <mike@vorburger.ch>
2018-02-14 17:02:51 +0100

    add NeutronSimpleDistributionTest
    TODO: Get filter working (easy), get Neutron IT to pass this! (...)
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ src/main/java/org/opendaylight/neutron/simple/NeutronWiring.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.simple;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.ws.rs.core.Application;
+import org.opendaylight.infrautils.web.WebContextProvider;
+import org.opendaylight.neutron.northbound.api.NeutronNorthboundRSApplication;
+
+public class NeutronWiring extends AbstractModule {
+
+    @Override
+    protected void configure() {
+    }
+
+    @Provides
+    Servlet jaxRSNeutron(WebContextProvider webContextProvider) throws ServletException {
+        Application app = new NeutronNorthboundRSApplication();
+        Servlet jaxRSNeutronServlet = new ServletContainer(app);
+        // /controller/nb/v2/neutron is from neutron.northbound-api/pom.xml's <Web-ContextPath>
+        webContextProvider.newWebContext("/controller/nb/v2/neutron", false /* no HTTP sessions needed */)
+             // following is from neutron.northbound-api web.xml
+            .registerServlet("/*", "JAXRSNeutron", jaxRSNeutronServlet);
+/*
+            .registerFilter("/*", "cross-origin-restconf", new CrossOriginFilter(), ImmutableMap.of(
+                        "allowedOrigins", "*", "allowedMethods",
+                        "GET,POST,OPTIONS,DELETE,PUT,HEAD",
+                        "allowedHeaders", "origin, content-type, accept, authorization"));
+*/
+        // TODO add security stuff... AAA Shiro - or another one?
+        return jaxRSNeutronServlet;
+    }
+
+}
---
+++ src/test/java/org/opendaylight/neutron/simple/test/NeutronSimpleDistributionTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.neutron.simple.test;
+
+import javax.inject.Inject;
+import javax.servlet.Servlet;
+import org.junit.Rule;
+import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
+import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule2;
+import org.opendaylight.infrautils.simple.ReadyWiring;
+import org.opendaylight.infrautils.simple.testutils.AbstractSimpleDistributionTest;
+import org.opendaylight.infrautils.simple.web.impl.WebWiring;
+import org.opendaylight.neutron.simple.NeutronWiring;
+
+/**
+ * Netvirt full distribution component test.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class NeutronSimpleDistributionTest extends AbstractSimpleDistributionTest {
+
+    public @Rule GuiceRule2 guice = new GuiceRule2(
+            NeutronWiring.class, WebWiring.class, ReadyWiring.class, AnnotationsModule.class);
+
+    // It's stupid that this is required...
+    @SuppressWarnings("unused")
+    private @Inject Servlet neutronJaxRsServlet;
+
+}
