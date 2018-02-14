c48c0d23104c7fe164c01749e0b7c2ff1839de9d
Michael Vorburger <vorburger@redhat.com>
2018-02-14 16:17:36 +0100

    rename package simple.jetty.test to simple.web.test

--- src/test/java/org/opendaylight/infrautils/simple/web/test/JettyLauncherTest.java
+++ src/test/java/org/opendaylight/infrautils/simple/web/test/JettyLauncherTest.java
@@ -5,7 +5,7 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.infrautils.simple.jetty.test;
+package org.opendaylight.infrautils.simple.web.test;
 
 import static com.google.common.truth.Truth.assertThat;
 import static java.nio.charset.StandardCharsets.US_ASCII;
