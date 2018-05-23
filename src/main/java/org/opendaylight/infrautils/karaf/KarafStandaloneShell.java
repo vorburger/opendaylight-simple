632b48126150f2b37bfb836d8300ba383f6a16d5
Michael Vorburger <mike@vorburger.ch>
2018-05-23 19:04:40 +0200

    prepare Karaf CLI shell command integration (just basis; no actual cmds)
    see https://github.com/vorburger/ch.vorburger.karaf.simple
    
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

---
+++ src/main/java/org/opendaylight/infrautils/karaf/KarafStandaloneShell.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.infrautils.karaf;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import org.apache.karaf.shell.api.action.lifecycle.Manager;
+import org.apache.karaf.shell.api.console.Session;
+import org.apache.karaf.shell.impl.action.command.ManagerImpl;
+
+/**
+ * Karaf standalone shell, with programmatic instead of file-based command registration.
+ *
+ * @author Michael Vorburger.ch
+ */
+public class KarafStandaloneShell {
+
+    private final InnerMain karafMain = new InnerMain();
+
+    private final Queue<Class<?>> actionClasses = new ConcurrentLinkedDeque<>();
+
+    /**
+     * Register a service.
+     * If the given class is an {@link org.apache.karaf.shell.api.action.Action},
+     * a {@link org.apache.karaf.shell.api.console.Command} will be created and registered,
+     * else, an instance of the class will be created, injected and registered.
+     *
+     * @param clazz the Action class to register.
+     */
+    // as in org.apache.karaf.shell.api.action.lifecycle.Manager.register(Class<?>), but without unregister!
+    public KarafStandaloneShell register(Class<?> clazz) {
+        actionClasses.add(clazz);
+        return this;
+    }
+
+    public void run() throws Exception {
+        karafMain.run(new String[0]);
+    }
+
+    private class InnerMain extends org.apache.karaf.shell.impl.console.standalone.Main {
+        @Override
+        protected void discoverCommands(Session session, ClassLoader cl, String resource) {
+            Manager manager = new ManagerImpl(session.getRegistry(), session.getFactory().getRegistry(), true);
+            for (Class<?> clazz : actionClasses) {
+                manager.register(clazz);
+            }
+        }
+    }
+}
