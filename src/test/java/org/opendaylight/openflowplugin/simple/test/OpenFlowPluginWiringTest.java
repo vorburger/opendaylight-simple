5b72476246387b6be257b718de4aed85666b9623
Michael Vorburger <mike@vorburger.ch>
2018-12-19 01:05:00 +0100

    very minor clean ups
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- src/test/java/org/opendaylight/openflowplugin/simple/test/OpenFlowPluginWiringTest.java
+++ src/test/java/org/opendaylight/openflowplugin/simple/test/OpenFlowPluginWiringTest.java
@@ -17,12 +17,14 @@ import org.opendaylight.infrautils.simple.ReadyWiring;
 import org.opendaylight.infrautils.simple.testutils.AbstractSimpleDistributionTest;
 import org.opendaylight.infrautils.web.WebWiring;
 import org.opendaylight.openflowplugin.simple.OpenFlowPluginWiring;
+import org.opendaylight.serviceutils.simple.ServiceUtilsWiring;
 
 public class OpenFlowPluginWiringTest extends AbstractSimpleDistributionTest {
 
     private static final GuiceClassPathBinder CLASS_PATH_BINDER = new GuiceClassPathBinder("org.opendaylight");
 
-    public @Rule GuiceRule guice = new GuiceRule(new OpenFlowPluginWiring(CLASS_PATH_BINDER), new ControllerWiring(),
-            new DiagStatusWiring(), new WebWiring(), new ReadyWiring(), new AnnotationsModule());
+    public @Rule GuiceRule guice = new GuiceRule(new OpenFlowPluginWiring(CLASS_PATH_BINDER), new ServiceUtilsWiring(),
+            new ControllerWiring(), new DiagStatusWiring(), new WebWiring(), new ReadyWiring(),
+            new AnnotationsModule());
 
 }
