21a5b968507125e7a0e30f0e34e0b091cd3c39cd
Michael Vorburger <vorburger@redhat.com>
2018-12-21 16:01:05 +0100

    add @Inject PacketProcessingService to OpenFlowPluginModuleTest
    this requires https://jira.opendaylight.org/browse/OPNFLWPLUG-1059

--- src/test/java/org/opendaylight/openflowplugin/simple/test/OpenFlowPluginModuleTest.java
+++ src/test/java/org/opendaylight/openflowplugin/simple/test/OpenFlowPluginModuleTest.java
@@ -23,6 +23,7 @@ import org.opendaylight.infrautils.web.WebModule;
 import org.opendaylight.openflowplugin.api.openflow.OpenFlowPluginProvider;
 import org.opendaylight.openflowplugin.simple.OpenFlowPluginModule;
 import org.opendaylight.serviceutils.simple.ServiceUtilsModule;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingService;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflow.provider.config.rev160510.OpenflowProviderConfig;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.openflowplugin.app.forwardingrules.manager.config.rev160511.ForwardingRulesManagerConfig;
 
@@ -34,6 +35,7 @@ public class OpenFlowPluginModuleTest extends AbstractSimpleDistributionTest {
             new ServiceUtilsModule(), new InMemoryControllerModule(), new DiagStatusModule(), new WebModule(),
             new ReadyModule(), new AnnotationsModule());
 
+    @Inject PacketProcessingService packetProcessingService; // OPNFLWPLUG-1059
     @Inject OpenFlowPluginProvider ofpProvider;
     @Inject OpenflowProviderConfig ofpConfig;
     @Inject ForwardingRulesManagerConfig frmConfig;
