ba26713f289d36e35524cddf139497a6c088c0dd
Michael Vorburger <mike@vorburger.ch>
2018-12-21 13:35:04 +0100

    make InMemoryControllerModuleTest actually test DataBroker
    Signed-off-by: Michael Vorburger <mike@vorburger.ch>

--- src/test/java/org/opendaylight/controller/simple/test/InMemoryControllerModuleTest.java
+++ src/test/java/org/opendaylight/controller/simple/test/InMemoryControllerModuleTest.java
@@ -7,7 +7,10 @@
  */
 package org.opendaylight.controller.simple.test;
 
+import javax.inject.Inject;
 import org.junit.Rule;
+import org.junit.Test;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.simple.InMemoryControllerModule;
 import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
 import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule2;
@@ -17,4 +20,9 @@ public class InMemoryControllerModuleTest extends AbstractSimpleDistributionTest
 
     public @Rule GuiceRule2 guice = new GuiceRule2(InMemoryControllerModule.class, AnnotationsModule.class);
 
+    @Inject DataBroker dataBroker;
+
+    @Test public void testDataBroker() {
+        dataBroker.newReadWriteTransaction().commit();
+    }
 }
