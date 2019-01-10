0c2879277f90443d229ac6be6eafef46aa4501e9
Michael Vorburger <vorburger@redhat.com>
2019-01-10 04:14:00 +0100

    replace TestHttpClient with upstream TestWebClient
    requires https://git.opendaylight.org/gerrit/#/c/79382/

--- src/test/java/org/opendaylight/neutron/simple/test/NeutronModuleTest.java
+++ src/test/java/org/opendaylight/neutron/simple/test/NeutronModuleTest.java
@@ -15,11 +15,11 @@ import javax.inject.Inject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.opendaylight.aaa.web.WebServer;
+import org.opendaylight.aaa.web.testutils.TestWebClient;
 import org.opendaylight.controller.simple.InMemoryControllerModule;
 import org.opendaylight.infrautils.inject.guice.testutils.AnnotationsModule;
 import org.opendaylight.infrautils.inject.guice.testutils.GuiceRule;
 import org.opendaylight.infrautils.simple.testutils.AbstractSimpleDistributionTest;
-import org.opendaylight.infrautils.testutils.TestHttpClient;
 import org.opendaylight.infrautils.web.WebModule;
 import org.opendaylight.neutron.simple.NeutronModule;
 
@@ -34,9 +34,9 @@ public class NeutronModuleTest extends AbstractSimpleDistributionTest {
             NeutronModule.class, InMemoryControllerModule.class, WebModule.class, AnnotationsModule.class);
 
     @Inject WebServer webServer;
-    @Inject TestHttpClient http;
+    @Inject TestWebClient http;
 
     @Test public void testNeutron() throws IOException {
-        assertThat(http.responseCode(GET, "/controller/nb/v2/neutron/networks")).isEqualTo(200);
+        assertThat(http.request(GET, "/controller/nb/v2/neutron/networks").getStatus()).isEqualTo(200);
     }
 }
