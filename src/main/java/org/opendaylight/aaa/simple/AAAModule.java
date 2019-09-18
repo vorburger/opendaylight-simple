41cc85057f0a843b66b9a0506bc655cf0caeb6a7
Tejas Nevrekar <tnevrekar@luminanetworks.com>
2019-09-18 15:55:47 +0530

    NETCONF: Added support for NETCONF to ODL-SIMPLE

--- src/main/java/org/opendaylight/aaa/simple/AAAModule.java
+++ src/main/java/org/opendaylight/aaa/simple/AAAModule.java
@@ -7,7 +7,18 @@
  */
 package org.opendaylight.aaa.simple;
 
+import javax.annotation.Nullable;
+import javax.inject.Singleton;
+
+import org.opendaylight.aaa.api.AuthenticationException;
+import org.opendaylight.aaa.api.Claim;
+import org.opendaylight.aaa.api.CredentialAuth;
+import org.opendaylight.aaa.api.PasswordCredentials;
+import org.opendaylight.aaa.shiro.tokenauthrealm.auth.ClaimBuilder;
+import org.opendaylight.aaa.shiro.tokenauthrealm.auth.PasswordCredentialBuilder;
+
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 
 public class AAAModule extends AbstractModule {
 
@@ -17,4 +28,22 @@ public class AAAModule extends AbstractModule {
         install(new ShiroModule());
     }
 
+    @Provides
+	@Singleton
+	CredentialAuth<PasswordCredentials> getPasswordCredentialAuth() {
+    	PasswordCredentials passwordCredentials = new PasswordCredentialBuilder().setUserName("admin").setPassword("admin").setDomain("").build();
+    	return new CredentialAuth<PasswordCredentials>() {
+
+			@Nullable
+			@Override
+			public Claim authenticate(PasswordCredentials cred) throws AuthenticationException {
+				if (cred.equals(passwordCredentials)) {
+					return new ClaimBuilder()
+							.setUser("admin")
+							.build();
+				}
+				return null;
+			}
+		};
+	}
 }
---
+++ src/main/java/org/opendaylight/netconf/simple/annotations/AggregatedNetconfOperationServiceFactoryMappers.java
@@ -0,0 +1,40 @@
+/*
+ * (C) 2019 Lumina Networks, Inc.
+ * 2077 Gateway Place, Suite 500, San Jose, CA 95110.
+ * All rights reserved.
+ *
+ * Use of the software files and documentation is subject to license terms.
+ */
+package org.opendaylight.netconf.simple.annotations;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+import org.opendaylight.netconf.mapping.api.NetconfOperationServiceFactory;
+
+/**
+ * PingPong {@link NetconfOperationServiceFactory} dependency injection annotation.
+ *
+ * <p>USAGE: <pre> {@literal @}Singleton
+ * public class Thing {
+ *     {@literal @}Inject
+ *     public Thing({@literal @}AggregatedNetconfOperationServiceFactoryMappers NetconfOperationServiceFactory listener) {
+ *         ...
+ *     }
+ * }</pre>
+ */
+@Qualifier
+@Documented
+@Retention(RUNTIME)
+@Target({ PARAMETER, METHOD, FIELD })
+public @interface AggregatedNetconfOperationServiceFactoryMappers {
+
+}
